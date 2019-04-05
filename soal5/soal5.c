07e1464ae906f3a011f9a951a5f14cd33bc90cae
Rana Wijdan Naim <45732575+rwnaim@users.noreply.github.com>
2019-04-05 20:30:22 +0700

    Update soal5.c

--- soal5/soal5.c
+++ soal5/soal5.c
@@ -21,3 +21,24 @@ int mandi=1;
 int makan=1;
 int *mandiptr=&mandi;
 int *makanptr=&makan;
+
+int battle=0;
+int *battleptr=&battle;
+int enemyhealt=100;
+int* enemyptr=&enemyhealt;
+
+int stokmakanan=2;
+int *stokmakananptr=&stokmakanan;
+
+
+
+pthread_t tid;
+
+typedef struct 
+{
+    time_t lst_makan;
+    time_t lst_mandi;
+}timestat;
+
+timestat myMonster;
+timestat *jam = &myMonster;
